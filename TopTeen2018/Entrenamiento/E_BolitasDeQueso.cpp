14d34adf78d39c039c8d1e540dc17ecd4ba22ef9
Diego García Rodríguez del Campo <diegomysterio@gmail.com>
2018-10-08 13:30:31 -0500

    Create E_BolitasDeQueso.cpp

---
+++ TopTeen2018/Entrenamiento/E_BolitasDeQueso.cpp
@@ -0,0 +1,11 @@
+//  Copyright © 2018 Diego Garcia Rodriguez del Campo. All rights reserved.
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    cout << 3*N + 2 << "\n";
+    return 0;
+}
