56c69c616dfe3450d19dd690ccf4ac95ddd84f6b
Diego García Rodríguez del Campo <diegomysterio@gmail.com>
2018-10-17 14:24:09 -0500

    Update G.cpp

--- TopTeen2018/Semana1/G.cpp
+++ TopTeen2018/Semana1/G.cpp
@@ -2,22 +2,20 @@
 #include<iostream>
 using namespace std;
 
-string s1, s2;
-
-int val( string s )
-{
-    int ans = 1;
-    for( int i = 0; i < s.size(); i ++ )
-        ans = ( ans * ( s[i] - 'A' + 1 ) ) % 47;
-    return ans;
-}
+int N;
+int a[105];
+int b[105];
 
 int main()
 {
-    cin >> s1 >> s2;
-    if( val( s1 ) == val( s2 ) )
-        cout << "GO\n";
-    else
-        cout << "STAY\n";
+    cin >> N;
+    for( int i = 0; i < N; i ++ )
+        cin >> a[i];
+        
+    for( int i = 0; i < N; i ++ )
+        cin >> b[i];
+        
+    for( int i = 0; i < N; i ++ )
+        cout << a[i] + b[i] << " ";
     return 0;
 }
