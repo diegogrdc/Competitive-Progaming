62ace72817b3d32e15c2e03091729105cceabecc
Diego García Rodríguez del Campo <diegomysterio@gmail.com>
2018-10-17 14:09:36 -0500

    Create B.cpp

---
+++ Semana1/B.cpp
@@ -0,0 +1,12 @@
+//  Copyright © 2018 Diego Garcia Rodriguez del Campo. All rights reserved.
+#include<iostream>
+using namespace std;
+
+string s;
+int main()
+{
+    cin >> s;
+    for( int i = 0; i < s.size(); i ++ )
+        cout << s[i] << "\n";
+    return 0;
+}
